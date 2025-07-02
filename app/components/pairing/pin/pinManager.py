ae5ecbe693a04bf9a643da32c7f4569f567c132b
SoliGabiAnn <166647049+SoliGabiAnn@users.noreply.github.com>
2025-07-02 12:39:51 +0200

    PIN from TOTP to HOTP

--- app/components/pairing/pin/pinManager.py
+++ app/components/pairing/pin/pinManager.py
@@ -1,22 +1,31 @@
 import pyotp
+from flask import flash
+from random import randint
+from ..pairingConfig import PIN_COUNTER_THERSHOLD, PIN_COUNTER_LIMIT
 
 class PinManager:
     '''
     Service offering server-side otp pins with token regenerated with each restart
     '''
     def __init__(self):
-        self.secret = pyotp.random_base32()
-        self.totp = pyotp.TOTP(self.secret)
-        self.used_pin = ''
+        self.hotp = pyotp.HOTP('base32secret3232')
+        # self.counter = randint(PIN_COUNTER_THERSHOLD,PIN_COUNTER_LIMIT)
+        # self.used_pin = ''
 
     def get_pin(self):
-        return self.totp.now()
+        self.counter= self.new_counter()
+        return self.hotp.at(self.counter)
 
     def check_pin(self, pin):
-        ok = pin == self.totp.now() and pin != self.used_pin
-        if ok:
-            self.used_pin = pin
-        return ok
+        if self.hotp.verify(pin, self.counter):
+            self.counter=self.new_counter()
+            return True
+        flash("Wrong PIN given. Start pairing again")
+        self.counter=self.new_counter()
+        return False
+    
+    def new_counter(self):
+        return randint(PIN_COUNTER_THERSHOLD,PIN_COUNTER_LIMIT)
     
 
 pin = PinManager()
